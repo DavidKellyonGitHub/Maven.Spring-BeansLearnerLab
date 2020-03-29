c339ce8c11c8e167cfcce1c69066cbd0a58ffe09
Dave Kelly <kelly.david.t@gmail.com>
2020-03-29 14:11:46 -0400

    part 9 complete

--- src/main/java/com/beanslearner/beanslearner/Classroom.java
+++ src/main/java/com/beanslearner/beanslearner/Classroom.java
@@ -1,4 +1,18 @@
 package com.beanslearner.beanslearner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Classroom {
+    Instructors instructors;
+    Students students;
+
+    public Classroom(Instructors instructors, Students students){
+        this.instructors = instructors;
+        this.students = students;
+    }
+
+    public void hostLecture(Teacher teacher, double numberOfHours){
+        teacher.lecture(students.personList, numberOfHours);
+    }
 }
